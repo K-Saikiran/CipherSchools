d4146449d7e846618655a9e0bbf9930d1821be59
K-Saikiran <saikirankandimalla90@gmail.com>
2022-02-02 22:05:44 +0530

    Initail commit

---
+++ Assignment 2/todo/src/components/ToDoList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const TodoList = ({ todolist, deleteHandler }) => {
+  return (
+    <div>
+      {todolist.map((todo, index) => (
+        <div key={index}>
+          <h5>
+            {todo} &nbsp;{" "}
+            <button
+              className="btn btn-danger"
+              bsSize="small"
+              onClick={() => deleteHandler(index)}
+            >
+              Delete
+            </button>
+          </h5>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
